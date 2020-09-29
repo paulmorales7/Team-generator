7eb21f65bdf254ff5d328db8f65a8fbd4aea3c34
Paul Morales <paulginga@hotmail.com>
2020-09-29 12:32:25 -0500

    updated

--- Output/generateHtml.js
+++ Output/generateHtml.js
@@ -26,43 +26,43 @@ function generateHtml(data) {
     
                     <div class="card manager-card">
                         <div class="card-header">
-                            <h2 class="card-title">{{ data.name }}</h2>
-                            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>{{ role }}</h3>
+                            <h2 class="card-title">${data.name}</h2>
+                            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${data.role}</h3>
                         </div>
                         <div class="card-body">
                             <ul class="list-group">
                                 <li class="list-group-item">ID: {{ data.id }}</li>
-                                <li class="list-group-item">Email: <a href="mailto:{{ data.email }}">{{ data.email }}</a></li>
-                                <li class="list-group-item">Office number: {{ data.officeNumber }}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                                <li class="list-group-item">Office number: ${data.officeNumber}</li>
                             </ul>
                         </div>
                     </div>
     
                     <div class="card intern-card">
                         <div class="card-header">
-                            <h2 class="card-title">{{ data.name }}</h2>
-                            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>{{ data.role }}</h3>
+                            <h2 class="card-title">${data.name}</h2>
+                            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${data.role}</h3>
                         </div>
                         <div class="card-body">
                             <ul class="list-group">
-                                <li class="list-group-item">ID: {{ data.id }}</li>
-                                <li class="list-group-item">Email: <a href="mailto:{{ data.email }}">{{ data.email }}</a></li>
-                                <li class="list-group-item">School: {{ data.school }}</li>
+                                <li class="list-group-item">ID: ${data.id}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                                <li class="list-group-item">School: ${data.school}</li>
                             </ul>
                         </div>
                     </div>
     
                     <div class="card engineer-card">
                         <div class="card-header">
-                            <h2 class="card-title">{{ data.name }}</h2>
-                            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{{ data.role }}</h3>
+                            <h2 class="card-title">${data.name}</h2>
+                            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${data.role}</h3>
                         </div>
                         <div class="card-body">
                             <ul class="list-group">
-                                <li class="list-group-item">ID: {{ data.id }}</li>
-                                <li class="list-group-item">Email: <a href="mailto:{{ data.email }}">{{ data.email }}</a></li>
-                                <li class="list-group-item">GitHub: <a href="https://github.com/{{ data.github }}"
-                                        target="_blank" rel="noopener noreferrer">{{ data.github }}</a></li>
+                                <li class="list-group-item">ID: ${data.id}</li>
+                                <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                                <li class="list-group-item">GitHub: <a href="https://github.com/${data.github}"
+                                        target="_blank" rel="noopener noreferrer">${data.github}</a></li>
                             </ul>
                         </div>
                     </div>
